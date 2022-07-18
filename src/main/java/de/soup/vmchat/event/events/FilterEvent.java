b83416377689f03bb0df85aecdbdea21bbf3f64b
SoupIDE <soupde@web.de>
2022-07-18 21:53:05 +0200

    Fixed Filtering

--- src/main/java/de/soup/vmchat/event/events/FilterEvent.java
+++ src/main/java/de/soup/vmchat/event/events/FilterEvent.java
@@ -3,6 +3,7 @@ package de.soup.vmchat.event.events;
 import de.soup.vmchat.VMChat;
 import de.soup.vmchat.config.configs.filter.elements.ChatFilters;
 import de.soup.vmchat.config.configs.filter.elements.FilterType;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -15,7 +16,7 @@ public class FilterEvent {
     {
         for(ChatFilters.ChatFilter filter : VMChat.getFilterConfig().getSettings().getChatFilters())
         {
-            if(matchesMessage(event.getMessage().getString(),filter.getPattern()))
+            if(matchesMessage(TextFormatting.stripFormatting(event.getMessage().getString()),filter.getPattern()))
             {
                 if(filter.getType() == FilterType.IGNORE || filter.getType() == FilterType.SECOND_CHAT) event.setCanceled(true);
                 if(filter.getType() == FilterType.SECOND_CHAT || filter.getType() == FilterType.COPY_SECOND_CHAT) VMChat.getVars().getSecondChat().addMessage(event.getMessage());
